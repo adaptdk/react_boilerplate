bafeb7278b07982616a9ecf4b72379ad6c020007
mads-thines <mads.thines@adaptagency.com>
2018-10-30 15:22:21 +0100

    Cleaned up uneccessary yarn install hook

--- config/setup/setup.js
+++ config/setup/setup.js
@@ -1,7 +1,5 @@
 /* eslint-disable */
 const prompt = require('prompt');
-const { exec } = require('child_process');
-    exec('yarn install');
 
 // Actions
 const {
@@ -46,9 +44,6 @@ const project = {
 getProjectName(project, () => {
   // Then Get Packages
   getPackages(project, () => {
-    // Silently in the background, start installing modules through yarn.
-    exec('yarn install');
-
     // Configure Git - Removing / Adding
     setupGit(project, () => {
       console.log(`
