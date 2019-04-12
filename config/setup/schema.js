f276f95470bc1d7999ba1f8a58199b8999f65e7d
mads-thines <mads.thines@adaptagency.com>
2019-04-12 16:12:02 +0200

    Added update

--- config/setup/schema.js
+++ config/setup/schema.js
@@ -1,20 +1,20 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 
-const { isYes } = require('./utils');
+const { isYes } = require("./utils");
 
 // The Questions
 const schema = {
   name: {
     properties: {
       machine: {
-        description: 'Machine name [only lowercase]',
-        message: 'Name must be only be lowercase letters',
+        description: "Machine name [only lowercase]",
+        message: "Name must be only be lowercase letters",
         pattern: /^[a-z]+$/,
         required: true,
       },
       title: {
-        description: 'Project title [only letters]',
-        message: 'Name must be only letters',
+        description: "Project title [only letters]",
+        message: "Name must be only letters",
         pattern: /^[a-zA-Z-\s]+$/,
         required: true,
       },
@@ -23,33 +23,22 @@ const schema = {
   packages: {
     properties: {
       package: {
-        message: 'You must select a package by entering it\'s number [0-9]',
-        description: 'Which package do you want to setup? [1 is default]',
+        message: "You must select a package by entering it's number [0-9]",
+        description: "Which package do you want to setup? [1 is default]",
         pattern: /[0-9]/,
-        default: '1',
+        default: "1",
         required: true,
       },
     },
   },
   git: {
     properties: {
-      removeLocal: {
-        description: 'Do you want to remove the boilerplate git repository? [y/n]',
-        message: 'Answer with either yes, y, no or n.',
-        pattern: /^(yes|y|no|n)/,
-        required: true,
-      },
-      addOwnRepo: {
-        description: 'Awesome! Would you like to add your own empty git repo? [y/n]',
-        message: 'Answer with either yes, y, no or n.',
-        pattern: /^(yes|y|no|n)/,
-        ask: () => prompt.history('removeLocal') && isYes(prompt.history('removeLocal').value),
-      },
       ownRepo: {
-        description: 'Alright, what\'s the SSH url for your git repo? [git@github.com:user/repo.git]',
+        description:
+          "SSH Url",
         pattern: /^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+).git$/,
-        message: 'You need to enter a valid git repository URL. like [git@github.com:user/repo.git]',
-        ask: () => prompt.history('addOwnRepo') && isYes(prompt.history('addOwnRepo').value),
+        message:
+          "You need to enter a valid git repository URL. like [git@github.com:user/repo.git]",
       },
     },
   },
