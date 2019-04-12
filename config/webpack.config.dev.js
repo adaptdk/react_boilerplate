6886018197dd0bea524245645d90459b9fa7bafc
mads-thines <mads.thines@adaptagency.com>
2019-04-12 12:03:15 +0200

    Added .babelrc and hot-loader/react-dom

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -5,17 +5,25 @@ const htmlWebpackPlugin = require("html-webpack-plugin");
 const webpackDeleteAfterEmit = require("webpack-delete-after-emit");
 const miniCssExtractPlugin = require("mini-css-extract-plugin");
 const rewireReactHotLoader = require("react-app-rewire-hot-loader");
+const { useBabelRc } = require("customize-cra");
 
 // Paths
 const paths = require("./paths");
 
 module.exports = function(config, isDev, settings) {
-  /*
-   * Insert your development specific configuration here.
-   */
-
   // Hot Loader
   config = rewireReactHotLoader(config, "development");
+
+  // Use .babelrc
+  useBabelRc();
+
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.resolve.alias,
+      "react-dom": "@hot-loader/react-dom",
+    },
+  };
 
   config.plugins = [
     ...config.plugins,
