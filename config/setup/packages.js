c9ecc9e865b8e841b7ab609b679ab40705bd5ecb
mads-thines <mads.thines@adaptagency.com>
2019-04-12 15:45:37 +0200

    Merge

--- config/setup/packages.js
+++ config/setup/packages.js
@@ -1,7 +1,5 @@
 // Utilities
-const {
-  underline,
-} = require('./utils');
+const { underline } = require("./utils");
 
 // Read more about the different packages here: https://github.com/adaptdk/react_boilerplate
 // The different packages
@@ -41,20 +39,21 @@ const packages = [
 const features = {
   redux: {
     predescription: `Read more about code structure here ${underline(
-      'https://redux.js.org/faq/codestructure#code-structure')}`,
-    description: 'Which code structure of Redux do you want?',
+      "https://redux.js.org/faq/codestructure#code-structure"
+    )}`,
+    description: "Which code structure of Redux do you want?",
     pattern: /[0-9]/,
-    message: 'The key you\'ve entered doesn\'t exists',
+    message: "The key you've entered doesn't exists",
     variants: [
       {
-        id: '1',
-        title: 'Ducks',
-        name: 'ducks',
+        id: "1",
+        title: "Ducks",
+        name: "ducks",
       },
       {
-        id: '2',
-        title: 'Function',
-        name: 'function',
+        id: "2",
+        title: "Function",
+        name: "function",
       },
     ],
   },
