27350885320e4556920a2d4af9325e27daae6694
Mads Thines <mads.thines@adaptagency.com>
2019-06-28 17:12:26 +0200

    Fixed Linting warning

--- src/utilities/store.ts
+++ src/utilities/store.ts
@@ -2,4 +2,5 @@ import { compose } from "redux";
 
 import { isDev } from "utilities/development";
 
-export const composeEnhancers = (isDev && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+export const composeEnhancers =
+  (isDev && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
