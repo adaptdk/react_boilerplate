a2adcc92215240e00a3065a4c0090d4108f1e5ef
Mads Thines <mads.thines@adaptagency.com>
2019-07-09 14:09:50 +0200

    Added Widgets variant to setup

--- config/setup/packages.js
+++ config/setup/packages.js
@@ -3,30 +3,42 @@
 // Utilities
 const { dim } = require("./utilities");
 
-const urlRegExp = /htt(ps|p).*(:|.)/;
-
 // Read more about the different packages here: https://github.com/adaptdk/react_boilerplate
 const packages = [
   {
+    type: "break",
+    title: "Typescript",
+  },
+  {
     id: "1",
-    title: "Base (TypeScript)",
+    title: "Base",
     branch: "typescript/base",
     features: [{ name: "proxy" }],
   },
   {
     id: "2",
-    title: "Complex (TypeScript)",
+    title: "Complex",
     branch: "typescript/complex",
     features: [{ name: "proxy" }],
   },
   {
     id: "3",
+    title: "Widgets",
+    branch: "typescript/widgets",
+    features: [{ name: "proxy" }],
+  },
+  {
+    type: "break",
+    title: "Regular",
+  },
+  {
+    id: "4",
     title: "Base",
     branch: "regular/base",
     features: [{ name: "proxy" }],
   },
   {
-    id: "4",
+    id: "5",
     title: "Complex",
     branch: "regular/complex",
     features: [{ name: "proxy" }],
@@ -49,7 +61,7 @@ const features = [
             "You can always enable later in config/config-overrides.js"
           )}`,
           description: "Local Env. URL",
-          pattern: urlRegExp,
+          pattern: /(no|^htt(ps|p).*(:|.))/,
           default: "no",
           required: true,
         },
