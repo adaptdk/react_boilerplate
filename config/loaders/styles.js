f6690e2c43ac2377499bf73db055e18444e49d22
mthines <mads.thines@adaptagency.com>
2019-08-20 08:41:00 +0200

    Update CSS Modules webpack loader for compatibility

--- config/loaders/styles.js
+++ config/loaders/styles.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 
-const ExtractCssChunks = require("extract-css-chunks-webpack-plugin");
-const autoprefixer = require("autoprefixer");
-const postcssCalc = require("postcss-calc");
-const postcssFlexbugs = require("postcss-flexbugs-fixes");
-const postcssInlineSvg = require("postcss-inline-svg");
-const paths = require("../paths");
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const postcssCalc = require('postcss-calc');
+const postcssFlexbugs = require('postcss-flexbugs-fixes');
+const postcssInlineSvg = require('postcss-inline-svg');
+const paths = require('../paths');
 
 // Style files regexes
 const cssRegex = /\.css$/;
@@ -16,13 +16,12 @@ const sassModuleRegex = /\.module\.(scss|sass)$/;
 // Config
 const _cssModuleLoader = isProd => [
   {
-    loader: "css-loader",
+    loader: 'css-loader',
     options: {
       sourceMap: !isProd,
-      modules: true,
-      localIdentName: isProd
-        ? "s[hash:base64:3]"
-        : "[name]__[local]--[hash:base64:3]",
+      modules: {
+        localIdentName: isProd ? 's[hash:base64:3]' : '[name]__[local]--[hash:base64:3]',
+      },
     },
   },
 ];
@@ -30,15 +29,15 @@ const _cssModuleLoader = isProd => [
 // PostCSS Config
 const _postcssConfig = [
   {
-    loader: "postcss-loader",
+    loader: 'postcss-loader',
     options: {
-      ident: "postcss",
+      ident: 'postcss',
       plugins: () => [
         autoprefixer(),
         postcssFlexbugs,
         postcssCalc,
         postcssInlineSvg({
-          path: paths.appSrc + "/assets/icons",
+          path: paths.appSrc + '/assets/icons',
         }),
       ],
     },
@@ -47,11 +46,11 @@ const _postcssConfig = [
 
 // Sass/Scss Config
 const _sassConfig = [
-  "sass-loader",
+  'sass-loader',
   {
-    loader: "sass-resources-loader",
+    loader: 'sass-resources-loader',
     options: {
-      resources: paths.appSrc + "/assets/styles/settings/settings.scss",
+      resources: paths.appSrc + '/assets/styles/settings/settings.scss',
     },
   },
 ];
@@ -68,19 +67,14 @@ const _extractCssPlugin = isProd => ({
 const sassLoader = isProd => ({
   test: sassRegex,
   exclude: sassModuleRegex,
-  use: [
-    isProd ? _extractCssPlugin(isProd) : "style-loader",
-    "css-loader",
-    ..._postcssConfig,
-    ..._sassConfig,
-  ],
+  use: [isProd ? _extractCssPlugin(isProd) : 'style-loader', 'css-loader', ..._postcssConfig, ..._sassConfig],
 });
 
 // Sass/Scss Module Loader (CSS Modules)
 const sassModuleLoader = isProd => ({
   test: sassModuleRegex,
   use: [
-    isProd ? _extractCssPlugin(isProd) : "style-loader",
+    isProd ? _extractCssPlugin(isProd) : 'style-loader',
     ..._cssModuleLoader(isProd),
     ..._postcssConfig,
     ..._sassConfig,
@@ -91,29 +85,16 @@ const sassModuleLoader = isProd => ({
 const cssLoader = isProd => ({
   test: cssRegex,
   exclude: cssModuleRegex,
-  use: [
-    isProd ? _extractCssPlugin(isProd) : "style-loader",
-    "css-loader",
-    ..._postcssConfig,
-  ],
+  use: [isProd ? _extractCssPlugin(isProd) : 'style-loader', 'css-loader', ..._postcssConfig],
 });
 
 // CSS Module Loader (CSS Modules)
 const cssModuleLoader = isProd => ({
   test: cssModuleRegex,
-  use: [
-    isProd ? _extractCssPlugin(isProd) : "style-loader",
-    ..._cssModuleLoader(isProd),
-    ..._postcssConfig,
-  ],
+  use: [isProd ? _extractCssPlugin(isProd) : 'style-loader', ..._cssModuleLoader(isProd), ..._postcssConfig],
 });
 
 // Collect and export
 module.exports = {
-  stylesLoaders: isProd => [
-    sassLoader(isProd),
-    sassModuleLoader(isProd),
-    cssLoader(isProd),
-    cssModuleLoader(isProd),
-  ],
+  stylesLoaders: isProd => [sassLoader(isProd), sassModuleLoader(isProd), cssLoader(isProd), cssModuleLoader(isProd)],
 };
